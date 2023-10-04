f6b0f632bad3358ba07b6856cb8e3df1cd87345b
Alexey Chernov <alexblackskja@gmail.com>
2023-10-04 14:04:41 +0300

    React_lesson4

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,10 +1,24 @@
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+  // cssLoader
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      "style-loader",
+      // Translates CSS into CommonJS
+      "css-loader",
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  };
+
+  // если мы не использовали typescript то нужен еще babel-loader
   const typeScriptLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
     exclude: /node_modules/,
   };
-  return [typeScriptLoader];
+  return [typeScriptLoader, cssLoader];
 }
