34356a8027ebfa6b994caf011dfaabc11e47806a
Alexey Chernov <alexblackskja@gmail.com>
2023-10-10 17:03:08 +0300

    i18n. Internationalization.Define plugin.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,17 +4,21 @@ import { useTheme } from "./providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
 import { NavBar } from "widgests/NavBar";
 import { SideBar } from "widgests/SideBar";
+import { Suspense } from "react";
+import { useTranslation } from "react-i18next";
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
-      <NavBar />
-      <div className="content-page">
-        <SideBar />
-        <AppRouter />
-      </div>
+      <Suspense fallback="">
+        <NavBar />
+        <div className="content-page">
+          <SideBar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
