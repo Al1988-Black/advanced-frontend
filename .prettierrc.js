b994b4224831c7d1ee51eaf03ce5cd590b82e718
Alexey Chernov <alexblackskja@gmail.com>
2023-10-13 23:58:17 +0300

    lesson19 Stylelint. Plugin for i18next

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,9 +3,18 @@ module.exports = {
     tabWidth: 4,
     semi: true,
     singleQuote: true,
-    parser: 'flow',
+    // parser: 'flow',
     printWidth: 80,
     // jsxSingleQuote: true,
     // arrowParens: 'always',
     // max-len: ['error', 140, 2],
+    overrides: [
+        {
+            files: '*.scss',
+            options: {
+                singleQuote: false,
+                printWidth: 200,
+            },
+        },
+    ],
 };
--- src/widgests/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgests/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,10 +1,9 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import cls from './LangSwitcher.module.scss';
 
 interface LangSwitcherProps {
-  className?: string;
+    className?: string;
 }
 
 export const LangSwitcher = ({ className }: LangSwitcherProps) => {
@@ -15,7 +14,7 @@ export const LangSwitcher = ({ className }: LangSwitcherProps) => {
     return (
         <Button
             theme={ThemeButton.CLEAR}
-            className={classNames(cls.LangSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
             onClick={toogle}
         >
             {t('Язык')}
